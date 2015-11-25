52a0d5aa07f8de43132bb540bbe5c8353673e71d
20141105053w <20141105053w@admindeimac-88.local>
2015-11-25 18:12:06 +0800

    Initial Commit

---
+++ real_add_imag/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  real_add_imag
+//
+//  Created by 20141105053w on 15/11/25.
+//  Copyright © 2015年 20141105053w. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
